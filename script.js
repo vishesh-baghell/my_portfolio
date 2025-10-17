7ab07c84e928c0ab48ab3e4e636c3372d6ec676c
vishesh-baghell <visheshbaghelx@gmail.com>
2025-10-17 08:29:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.getElementById("login-form").addEventListener("submit", function(e) {
+//       e.preventDefault();
+
+//       const username = document.getElementById("username").value;
+//       const password = document.getElementById("password").value;
+
+//       // Dummy login check
+//       if (username === "vishesh" && password === "vishesh0210") {
+//         // Redirect to portfolio.html
+//         window.location.href = "index.html";
+//       } else {
+//         alert("Invalid login. Try Username: admin, Password: admin");
+//       }
+//     });
